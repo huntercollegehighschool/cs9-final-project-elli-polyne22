7f7a1ac09f2640a04d0e1a7e7be8d797c0af9e3d
Ellison Polyne <104437128+elli-polyne22@users.noreply.github.com>
2022-06-17 22:34:03 +0000

    DONE

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-program =input("Which part(1, 2, 3, 4, or 5)? ")
+"""
+Name(s): Ellison and Josie
+Name of Project: Summer Themed Mad Lib
+"""
 
-while program not in ['1', '2', '3','4', '5']:
-  print
+print("Welcome to Josie and Ellison's Mad Libs game! This game is compromised of 4 short stories in which you will be asked to give either a noun, verb or adjective. These words will be placed into the story.")
+
+program = int(input("Which code (1 or 2 or 3 or 4)? "))
+if program == 1:
+  import page1
+if program == 2:
+  import page2
+if program == 3:
+  import page3
+if program == 4:
+  import page4
